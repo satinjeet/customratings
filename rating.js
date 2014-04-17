0d01a49d5ad93a738d21f4bad48e34c8037bf78e
satin <satin.jeet@gmail.com>
2014-04-17 12:47:58 +0530

    fix everything

---
+++ rating.js
@@ -0,0 +1,126 @@
+(function($) {
+    window.ratingObj = {
+        ":D":  7,
+        ":)":  6,
+        ":|":  5,
+        ":/":  4,
+        ':(':  3,
+        ":'(":  2,
+        ":@":  1,
+        "x_x":  0
+    }
+    var mainEle = ".ratings";
+
+    $.fn.rating = function(options) {
+
+        if (options === undefined) {
+            options = {};
+        }
+        /** create Rating ul list **/
+        ul = document.createElement("ul");
+        if (options.direction === "h") {
+            ul.className = "horizontal";            
+        }
+        for (var symbol in ratingObj) {
+            li = document.createElement("li");
+            li.innerHTML = symbol;
+            ul.appendChild(li);
+        }
+        this.find("div").html(ul);
+
+        /** add mouse listeners **/
+        this.mouseenter(options, bigger);
+        this.mouseleave(options, small);
+        this.on("click", options, rateIt);
+        this.on("close", options, small);
+        this.mouseenter(options, biggerH);
+        this.mouseleave(options, smallH);
+        this.on("click", options, rateItH);
+        this.on("close", options, smallH);
+
+        /** Handlers **/
+
+    }
+
+    var rateIt = function(e) {
+        if (e.data.direction == 'h') {
+            return;
+        }
+        $(this).stop();
+        if (e.target.nodeName.toLowerCase() === "li") {
+            li = $(e.target);
+            $(e.currentTarget).find("ul").animate({
+                "margin-top": -li.position().top
+            }, 500);
+            $(this).trigger("close");
+        }
+    }
+
+    var bigger = function(e, options) {
+        if (e.data.direction == 'h') {
+            return;
+        }
+        ul = $(this).find("ul");
+        $(this).stop();
+        ul.animate({
+            "margin-top": 0
+        }, 500);
+        $(this).animate({
+            height: ul.height()
+        }, 500)
+    }
+
+    var small = function(e) {
+        if (e.data.direction == 'h') {
+            return;
+        }
+        $(this).stop();
+        li = $(this).find("ul").find("li");
+        $(this).animate({
+            height: li[0].clientHeight
+        }, 500);
+    }
+
+    var rateItH = function(e) {
+        if (e.data.direction != 'h') {
+            return;
+        }
+        $(this).stop();
+        if (e.target.nodeName.toLowerCase() === "li") {
+            li = $(e.target);
+            $(e.currentTarget).find("ul").animate({
+                "margin-left": -li.position().left
+            }, 500);
+            $(this).trigger("close");
+        }
+    }
+
+    var biggerH = function(e, options) {
+        if (e.data.direction != 'h') {
+            return;
+        }
+        $(this).stop();
+        ul = $(this).find("ul");
+        li = $("li").last();
+
+        ul.animate({
+            "margin-left": 0
+        }, 500);
+        $(this).animate({
+            width: li.position().left + 50
+        }, 500)
+    }
+
+    var smallH = function(e) {
+        if (e.data.direction != 'h') {
+            return;
+        }
+        $(this).stop();
+        li = $(this).find("ul").find("li");
+        $(this).animate({
+            width: 50
+        }, 500);
+    }
+
+
+})(jQuery);
